64605710217541cba0e373b8a0887bcd9c10ff57
Another Name <s2307@e-mirim.hs.kr>
2025-08-28 15:00:01 +0900

    위젯 이름 변경

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,33 +29,35 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.tbGenre = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tbEmotion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbState = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.내역불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.도움말ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.프로젝트정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbResult = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.btnResult);
+            this.groupBox1.Controls.Add(this.tbGenre);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.tbEmotion);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.tbState);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.groupBox1.Location = new System.Drawing.Point(12, 40);
@@ -66,56 +68,22 @@
             this.groupBox1.Text = "정보 입력";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // groupBox2
+            // btnResult
             // 
-            this.groupBox2.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.groupBox2.Location = new System.Drawing.Point(410, 40);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(378, 398);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "결과보기";
+            this.btnResult.Font = new System.Drawing.Font("나눔스퀘어라운드 Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnResult.Location = new System.Drawing.Point(20, 330);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(343, 52);
+            this.btnResult.TabIndex = 6;
+            this.btnResult.Text = "결과보기";
+            this.btnResult.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // tbGenre
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(18, 54);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(218, 14);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "뇌 상태를 표현한다면?(OFF, 편안, 집중)";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(20, 69);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(343, 21);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(20, 139);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(343, 21);
-            this.textBox2.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(18, 124);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(295, 14);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "지금 가장 가까운 감정은?(기쁨, 슬픔, 우울, 분노, 피로)";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(20, 229);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(343, 21);
-            this.textBox3.TabIndex = 5;
+            this.tbGenre.Location = new System.Drawing.Point(20, 229);
+            this.tbGenre.Name = "tbGenre";
+            this.tbGenre.Size = new System.Drawing.Size(343, 21);
+            this.tbGenre.TabIndex = 5;
             // 
             // label3
             // 
@@ -127,15 +95,50 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "가장 듣고싶은 이야기는?\r\n(사랑, 우정, 모험, 감동, 코미디, 성장, 일상, 판타지, 도전)";
             // 
-            // button1
+            // tbEmotion
             // 
-            this.button1.Font = new System.Drawing.Font("나눔스퀘어라운드 Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.Location = new System.Drawing.Point(20, 330);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(343, 52);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "결과보기";
-            this.button1.UseVisualStyleBackColor = true;
+            this.tbEmotion.Location = new System.Drawing.Point(20, 139);
+            this.tbEmotion.Name = "tbEmotion";
+            this.tbEmotion.Size = new System.Drawing.Size(343, 21);
+            this.tbEmotion.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(18, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "지금 가장 가까운 감정은?(기쁨, 슬픔, 우울, 분노, 피로)";
+            // 
+            // tbState
+            // 
+            this.tbState.Location = new System.Drawing.Point(20, 69);
+            this.tbState.Name = "tbState";
+            this.tbState.Size = new System.Drawing.Size(343, 21);
+            this.tbState.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(18, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "뇌 상태를 표현한다면?(OFF, 편안, 집중)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbResult);
+            this.groupBox2.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox2.Location = new System.Drawing.Point(410, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(378, 398);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "결과보기";
             // 
             // menuStrip1
             // 
@@ -164,16 +167,16 @@
             this.내역불러오기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.내역불러오기ToolStripMenuItem.Text = "내역 불러오기";
             // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
             // 끝내기ToolStripMenuItem
             // 
             this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
             this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.끝내기ToolStripMenuItem.Text = "끝내기";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
             // 
             // 도움말ToolStripMenuItem
             // 
@@ -186,9 +189,17 @@
             // 프로젝트정보ToolStripMenuItem
             // 
             this.프로젝트정보ToolStripMenuItem.Name = "프로젝트정보ToolStripMenuItem";
-            this.프로젝트정보ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.프로젝트정보ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
             this.프로젝트정보ToolStripMenuItem.Text = "프로젝트 정보";
             this.프로젝트정보ToolStripMenuItem.Click += new System.EventHandler(this.프로젝트정보ToolStripMenuItem_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(6, 20);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(366, 372);
+            this.tbResult.TabIndex = 0;
             // 
             // Form1
             // 
@@ -202,6 +213,8 @@
             this.Text = "애니무드 프로젝트";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -212,14 +225,14 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox tbGenre;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbEmotion;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbState;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnResult;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 내역불러오기ToolStripMenuItem;
@@ -227,6 +240,7 @@
         private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 도움말ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 프로젝트정보ToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbResult;
     }
 }
 
